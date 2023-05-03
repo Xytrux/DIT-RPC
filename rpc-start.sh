7da476c8e869df9dec67f64fb05909e18c9229ba
Xytrux <helloworldpy103@gmail.com>
2023-05-03 09:40:34 -0700

    changed some text

--- rpc-start.sh
+++ rpc-start.sh
@@ -2,11 +2,22 @@
 
 cd
 if [ -d DIT-RPC ]; then
-    echo "Deleting DIT-RPC folder..."
-    rm -rf DIT-RPC
-    git clone https://github.com/Xytrux/DIT-RPC.git
-    cd DIT-RPC
-    python3 main.py
+    while true; do
+    read -p "Found another folder (DIT-RPC) Do you want to delete it? (y/n) " yn
+    case $yn in
+        [Yy]* ) echo "Deleting DIT-RPC folder..."; 
+        rm -rf DIT-RPC
+        git clone https://github.com/Xytrux/DIT-RPC.git
+        cd DIT-RPC
+        python3 main.py
+        break;;
+        [Nn]* ) echo "Using current folder..."; 
+        cd DIT-RPC
+        python3 main.py
+        exit;;
+        * ) echo "Please answer yes or no.";;
+        esac
+    done
 fi
 git clone https://github.com/Xytrux/DIT-RPC.git
 cd DIT-RPC
