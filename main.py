349680887f44f914bd132f638f1d5e4f2363da02
Xytrux <helloworldpy103@gmail.com>
2023-05-03 09:29:59 -0700

    Added the RPC script

---
+++ main.py
@@ -0,0 +1,21 @@
+from pypresence import Presence 
+import time
+
+start = int(time.time())
+client_id = "1103349211531063368" # put your client ID here
+RPC = Presence(client_id)
+RPC.connect()
+print("RPC Connected!")
+
+while True:
+    RPC.update(
+        large_image = "dit",
+        large_text = "Join the Discord Institute of Technology!",
+        #small_image = "",
+        #small_text = "",
+        details = "Talk about tech and education!",
+        #state = "",
+        start = start,
+        buttons = [{"label": "Join D.I.T!", "url": "https://discord.gg/WdRhvcdqhK"}]
+    )
+    time.sleep(60)
